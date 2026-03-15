a2d7d6d527f2ea43e23bc70e6e631fd97efca293
Ammar <dev.ammar15@gmail.com>
2026-03-16 01:45:41 +0500

    Fixing Lint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,14 +1,7 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-    es2021: true,
-  },
+  env: { node: true, es2021: true },
   extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {
-    // disable all rules for now to prevent errors
-  },
+  parserOptions: { ecmaVersion: "latest" },
+  rules: {}
 };
