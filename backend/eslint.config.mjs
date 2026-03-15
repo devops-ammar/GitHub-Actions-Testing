d3ce1984f2f3f74c58abb53f79aafadf3194c567
Ammar <dev.ammar15@gmail.com>
2026-03-16 01:36:43 +0500

    Backend Linting...

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    // disable all rules for now to prevent errors
+  },
+};
